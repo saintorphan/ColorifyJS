0f3a87fe0a1e9a057e2646ab6ff56c9e3e4c2c2e
saintorphan <128126151+saintorphan@users.noreply.github.com>
2024-02-19 02:01:50 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 console.log('Script Loaded...');
 // Simple paint function
 function paint(col) {
-    
+
     const circle = document.getElementById('circleID');
     circle.style = `background-color:${col}`;
-    
+
 }
 
-var colorCanvas = document.getElementById('color_canvas');
-var ColorCtx = colorCanvas .getContext('2d');  // This create a 2D context for the canvas
+const colorCanvas = document.getElementById('color_canvas');
+const ColorCtx = colorCanvas.getContext('2d');  // This create a 2D context for the canvas
 
 var color = 'rgba(0,0,255,1)';
-let gradientH = ColorCtx .createLinearGradient(0, 0, ColorCtx .canvas.width, 0);
+let gradientH = ColorCtx.createLinearGradient(0, 0, ColorCtx.canvas.width, 0);
 gradientH.addColorStop(0, '#fff');
 gradientH.addColorStop(1, color);
-ColorCtx .fillStyle = gradientH;
-ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, ColorCtx .canvas.height);
+ColorCtx.fillStyle = gradientH;
+ColorCtx.fillRect(0, 0, ColorCtx.canvas.width, ColorCtx.canvas.height);
 
 
 // Create a Vertical Gradient(white to black)
- let gradientV = ColorCtx .createLinearGradient(0, 0, 0, 300);
- gradientV.addColorStop(0, 'rgba(0,0,0,0)');
- gradientV.addColorStop(1, '#000');
- ColorCtx .fillStyle = gradientV;
- ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, 
- ColorCtx .canvas.height); 
- colorCanvas.addEventListener('click',function(event){
-    let x = event.clientX;  // Get X coordinate
-    let y = event.clientY;  // Get Y coordinate
-    const pixel = ColorCtx.getImageData(x,y,1,1)['data'];   // Read pixel Color
-   console.log(pixel);
-    const rgb = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`;
-    console.log(rgb);
+let gradientV = ColorCtx.createLinearGradient(0, 0, 0, 300);
+gradientV.addColorStop(0, 'rgba(0,0,0,0)');
+gradientV.addColorStop(1, '#000');
+ColorCtx.fillStyle = gradientV;
+ColorCtx.fillRect(0, 0, ColorCtx.canvas.width,
+    ColorCtx.canvas.height);
+
+
+colorCanvas.addEventListener('click', function (event) {
+
+    const bounding = colorCanvas.getBoundingClientRect();
+    const x = event.clientX - bounding.left;
+    const y = event.clientY - bounding.top;
+    const pixel = ColorCtx.getImageData(x, y, 1, 1);
+    const data = pixel.data;
+    console.log(pixel.data);
+    const rgbColor = `rgb(${data[0]},${data[1]},${data[2]})`;
     const circle = document.getElementById('circleID');
-    circle.style = `background-color:${rgb})`;
-   
- });
+    console.log(rgbColor);
+    //document.body.style.background = rgb;    // Set this color to body of the document
+    circle.style = `background-color:${rgbColor};)`;
+
+});
